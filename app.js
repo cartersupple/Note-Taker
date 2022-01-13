a46e9ed7909e86fe62c9d3a2f5635b5a6897337d
Carter Supple <Cartersupple123@gmail.com>
2022-01-12 21:46:55 -0800

    edits to app.js

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const express = require('express');
 const app = express();
 //URL PORT
 const PORT = process.env.PORT || 3000;
+//Require routes
+const api = require('./routes/api');
+const html = require('./routes/html');
 //app.path
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 // call the routes for the files
- require('./routes/api')(app);
- require('./routes/html')(app);
+app.use("/api", api);
+app.use("/", html);
 // calling the port to functions
 app.listen(PORT, () =>
   console.log(`API server now on port ${PORT}!`)
